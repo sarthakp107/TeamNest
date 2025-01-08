6f5dfaf8f658014cad7f6b52a68b19f12df21b6d
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-08 23:55:58 +1100

    navbar conditional rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,10 @@ import Temple from '../assets/temple.svg'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar() {
-
+    const{user} = useAuthContext();
     const{logout, error, isPending} = useLogout();
 
 const handleSubmit = (e) => {
@@ -22,15 +23,15 @@ const handleSubmit = (e) => {
                 <span>TeamNest</span>
             </li>
 
-            <li>
+           { !user && <li>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Signup</Link>
-            </li>
+            </li>}
 
-            <li>
+            {user && <li>
                 {!isPending && <button className='btn' onClick={handleSubmit}>Logout</button>}
                 {isPending && <button className='btn' onClick={handleSubmit} disabled>Loading</button>}
-            </li>
+            </li>}
         </ul>
     </div>
   )
