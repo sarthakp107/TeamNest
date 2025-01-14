40388ebb9f2a11ea4ba827155ca7b213d8c2782c
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-14 16:33:28 +1100

    Project filter add

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import OnlineUsers from '../../components/OnlineUsers'
 import ProjectList from '../../components/ProjectList';
 import { useCollection } from '../../hooks/useCollection'
+import ProjectFilter from './ProjectFilter';
+
 //styles
 import './Dashboard.css'
 
@@ -16,6 +18,7 @@ export default function Dashboard() {
         <div>
             <h2 className="page-title">Dashboard</h2>
             {error && <p className='error'>{error}</p>}
+            {documents && <ProjectFilter/>}
             {documents && <ProjectList projects={documents}/>}
         </div>
     )
